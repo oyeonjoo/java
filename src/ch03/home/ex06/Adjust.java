a12a316bea9f336c221c4f2b5f997d92dfb321b0
oyeonjoo <oyj0842@naver.com>
2023-04-16 23:24:11 +0900

    java

--- src/ch03/home/ex06/Adjust.java
+++ src/ch03/home/ex06/Adjust.java
@@ -15,19 +15,12 @@ public class Adjust {
 		System.out.println(result);
 		
 		/*
-		double d = 0.0;
-		
-		System.out.print("x: ");
-		d = sc.nextDouble();
-		System.out.println(d - d % 1.0);
-		 */
+		% 연산자를 이용해서, 실수값의 소수점 이하를 내림처리하라.
+		(casting), Math API사용 안 한다.
+		실수 값은 sc.nextDouble()로 입력한다.
+		------------------------------------------
+		예시) x: 3.1492
+			출력값: 3.0
+		*/
 	}
 }
-/*
-%연산자를 이용해서, 실수값의 소수점 이하를 내림처리하라.
-(casting), Math API사용 안 한다.
-실수 값은 sc.nextDouble()로 입력한다.
-------
-x: 3.1492
-3.0
-*/
